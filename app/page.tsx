bec3ee5e013084afc9f8a79ce8a888b3c901608d
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-03-14 13:01:01 +0530

    Headless UI search bar integrated.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
 
 export default function Home() {
   return (
@@ -8,6 +8,16 @@ export default function Home() {
         <div className="home__text-container">
           <h1 className="font-extrabold text-4xl">Car Catalogue</h1>
           <p className="font-medium">Explore the Cars you might Like</p>
+        </div>
+
+        {/* search bar */}
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filters-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
         </div>
       </div>
     </main>
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,16 @@ import Hero from "./Hero";
 import CustomButton from "./CustomButton";
 import Header from "./Header";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
-export { Hero, CustomButton, Header, Footer };
+export {
+  Hero,
+  CustomButton,
+  Header,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
