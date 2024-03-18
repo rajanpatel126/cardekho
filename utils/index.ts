b32f7060da4d7ce11dd4564646592abb59c2ba8d
Rajan Patel <88902043+rajanpatel126@users.noreply.github.com>
2024-03-18 11:55:09 +0530

    Car detail component added.

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
   const headers = new Headers();
 
@@ -31,3 +33,5 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {};
